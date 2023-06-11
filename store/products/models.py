1abcfb31389a6ea07de7fbefa3854ce3b1b09b78
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-06-11 14:32:58 +0300

    Working with models

--- store/products/models.py
+++ store/products/models.py
@@ -7,6 +7,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256, unique=True)
@@ -15,3 +18,6 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images/', blank=True, null=True)
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Product: {self.name} | Category: {self.category.name}'
