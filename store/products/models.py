b27a5a5f6885c14da178f1dad4475514eeb1ceed
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-06-11 16:57:57 +0300

    Models in controllers and templates

--- store/products/models.py
+++ store/products/models.py
@@ -16,7 +16,7 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products_images/', blank=True, null=True)
+    image_url = models.TextField(blank=True, null=True)
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
 
     def __str__(self):
