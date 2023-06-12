790e871e6ebc5006dab6398a23333da2f0535994
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-06-12 15:51:10 +0300

    Authorization controller

--- store/users/views.py
+++ store/users/views.py
@@ -1,10 +1,25 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib import auth
+from users.forms import UserLoginForm
+from django.urls import reverse
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 
 def registration(request):
