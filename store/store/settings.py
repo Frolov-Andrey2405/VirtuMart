9a7fb515d4cfc14ca0ea7d217658cf55536f9cf1
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-06-22 18:02:10 +0300

    Connecting Gmail to send emails

--- store/store/settings.py
+++ store/store/settings.py
@@ -139,4 +139,8 @@ LOGOUT_REDIRECT_URL = '/'
 
 # Sending emails
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_SSL = True
