7c672580f87018bcff948ef5ea027be61e0012af
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-06-23 16:45:23 +0300

    Cache handling, Redis, django-redis

--- store/store/settings.py
+++ store/store/settings.py
@@ -98,6 +98,18 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
+# Configure as cache backend
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
