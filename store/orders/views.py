fafb0033062fa0a168e14b2c293f8b87b9e8dfcd
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-06-23 18:07:33 +0300

    Transferring templates to the project

---
+++ store/orders/views.py
@@ -0,0 +1,6 @@
+from django.views.generic.edit import CreateView
+
+
+class OrderCreateView(CreateView):
+    template_name = 'orders/order-create.html'
+    form_class = None
--- store/store/settings.py
+++ store/store/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     # Apps
     'products',
     'users',
+    'orders',
 
     # OAuth
     'allauth',
