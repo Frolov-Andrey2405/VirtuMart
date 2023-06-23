b6f839b8df08cc34bc9e541979b567d7859edcfe
Frolov-Andrey2405 <senior.slender2015@gmail.com>
2023-06-23 23:47:17 +0300

    Stripe - webhook connection

--- store/orders/urls.py
+++ store/orders/urls.py
@@ -7,6 +7,9 @@ app_name = 'orders'
 
 urlpatterns = [
     path('order-create/', OrderCreateView.as_view(), name='order_create'),
-    path('order-success/', SuccessTemplateView.as_view(), name='order_success'),
-    path('order-canceled/', CanceledTemplateView.as_view(), name='order_canceled'),
+    path(
+        'order-success/', SuccessTemplateView.as_view(), name='order_success'),
+    path(
+        'order-canceled/', CanceledTemplateView.as_view(),
+        name='order_canceled'),
 ]
